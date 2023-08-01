be9198a8010b253280d4dae8c138a208fae9e0be
nikunj309 <103521892+nikunj309@users.noreply.github.com>
2023-08-01 22:39:43 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex justify-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          Ai-powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum autem laudantium a esse fugit quisquam vel ipsum, inventore obcaecati enim quidem totam temporibus dicta iusto.</p>
+    </section>
+  )
+}
+
+export default Home
